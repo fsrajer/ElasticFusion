3286175240d1108dd7f8e229860054abe8ec6189
Filip Srajer <filip@srajer.eu>
2016-11-17 12:00:44 +0100

    __CUDA_ARCH__ is not available on host. This code was always being evaluated to 512 anyway.

--- Core/src/Cuda/cudafuncs.cuh
+++ Core/src/Cuda/cudafuncs.cuh
@@ -52,11 +52,7 @@
 #ifndef CUDA_CUDAFUNCS_CUH_
 #define CUDA_CUDAFUNCS_CUH_
 
-#if __CUDA_ARCH__ < 300
 #define MAX_THREADS 512
-#else
-#define MAX_THREADS 1024
-#endif
 
 #include "containers/device_array.hpp"
 #include "types.cuh"
